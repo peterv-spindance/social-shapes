41cee66ad6b73c67f9e9b5b3e42107c4f9d3e37f
Peter Van Drunen <peter.vandrunen@spindance.com>
2019-03-25 13:30:03 -0400

    Initial ssr setup

--- src/website/src/index.js
+++ src/website/src/index.js
@@ -9,15 +9,22 @@ import { createLogger } from 'redux-logger';
 
 import rootReducer from './reducers';
 
+// Grab the state from a global variable injected into the server-generated HTML
+const preloadedState = window.__PRELOADED_STATE__
+
+// Allow the passed state to be garbage-collected
+delete window.__PRELOADED_STATE__
+
 const store = createStore(
   rootReducer,
+  preloadedState,
   applyMiddleware(
     thunk,
     createLogger()
   ),
 );
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <Root store={store} />,
   document.getElementById('root')
 );
